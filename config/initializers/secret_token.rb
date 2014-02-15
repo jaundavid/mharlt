7ad361bc3616bc50562754dd08fc8fe441eeb01c
Juan Uchuvo <juandaviid@gmail.com>
2014-02-15 10:44:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tuiter::Application.config.secret_key_base = 'd4d3e64bb491e64dee7d6afb64da5bf20acf28c2bdad68f6da46db490904c16b356eef0df09724ac0dbe71b652ecfee0c2491539c81886462aff1e6578bc6b3d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tuiter::Application.config.session_store :cookie_store, key: '_tuiter_session'
